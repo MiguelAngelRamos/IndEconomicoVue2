a1ed49434e6603dffd284e9e7ce1b134f271f444
Miguel Angel Ramos <return.infox@gmail.com>
2022-03-29 19:59:35 -0300

    antes del router

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        Indicadores Economicos
+      </a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/layout/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="container mt-5">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
